6fcf54139f5b63f31dc1b1615d459d0e54c45044
Igor Ieromenko <igor.ieromenko@3shape.com>
2019-10-15 16:09:32 +0300

    TicketsContainer is created

--- src/components/Tickets/tickets.js
+++ src/components/Tickets/tickets.js
@@ -1,28 +1,18 @@
 import React from 'react';
 
 import classes from './tickets.module.scss';
-import Axios from 'axios';
+
 import nextId from 'react-id-generator';
 
 const Tickets = props => {
-  if (!props.allTickets.searchId) {
-    Axios.get('https://front-test.beta.aviasales.ru/search').then(res => {
-      props.saveSearchId(res.data.searchId);
-    });
-  }
-
-  if (!props.allTickets.stop && props.allTickets.searchId) {
-    Axios.get(
-      `https://front-test.beta.aviasales.ru/tickets?searchId=${props.allTickets.searchId}`
-    ).then(res => props.saveTickets(res.data.tickets, res.data.stop));
-  }
-  console.log(props.allTickets.tickets);
   return (
-    <ol>
-      {props.allTickets.tickets.map(item => (
-        <li key={nextId()}>{item.price}</li>
-      ))}
-    </ol>
+    <div>
+      <ol>
+        {props.allTickets.map(item => (
+          <li key={nextId()}>{item.price}</li>
+        ))}
+      </ol>
+    </div>
   );
 };
 
--- src/components/Tickets/ticketsContainer.js
+++ src/components/Tickets/ticketsContainer.js
@@ -1,10 +1,33 @@
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Tickets from '.';
 import {
   searchIdActionCreator,
   getTicketsActionCreator
 } from '../../redux/tickets-reducer';
+import * as axios from 'axios';
 
+class TicketsContainer extends Component {
+  componentDidMount() {
+    axios
+      .get('https://front-test.beta.aviasales.ru/search')
+      .then(res => this.props.saveSearchId(res.data.searchId));
+  }
+
+  render() {
+    if (!this.props.allTickets.stop && this.props.allTickets.searchId) {
+      axios
+        .get(
+          `https://front-test.beta.aviasales.ru/tickets?searchId=${this.props.allTickets.searchId}`
+        )
+        .then(res => this.props.saveTickets(res.data.tickets, res.data.stop));
+    }
+
+    console.log(this.props.allTickets.tickets);
+
+    return <Tickets allTickets={this.props.allTickets.tickets} />;
+  }
+}
 const mapStateToProps = state => {
   return { allTickets: state.store };
 };
@@ -17,9 +40,7 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-const ticketsContainer = connect(
+export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(Tickets);
-
-export default ticketsContainer;
+)(TicketsContainer);
